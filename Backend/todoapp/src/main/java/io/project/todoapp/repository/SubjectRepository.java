a2e76ba0c257c6921c7f03b82bc12e8de8effc70
maxbu <max.buczek>
2023-06-19 20:49:59 +0200

    Implemented activating and deactivating tasks

---
+++ Backend/todoapp/src/main/java/io/project/todoapp/repository/SubjectRepository.java
@@ -0,0 +1,9 @@
+package io.project.todoapp.repository;
+
+import io.project.todoapp.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
